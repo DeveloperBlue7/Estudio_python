275d776a3d99462a3e58a10e43f2c123d3f33d77
DeveloperBlue7 <chris-developer@hotmail.com>
2022-10-29 13:04:24 -0500

    Primeras horas de Python

---
+++ Comprehensions - Funciones y manejo de errores/op_conjuntos.py
@@ -0,0 +1,31 @@
+# OPERACIONES CON CONJUNTOS
+from this import s
+
+
+set_a = {'Colombia', 'Mexico', 'Bolivia'}
+set_b = {'Peru', 'Bolivia'}
+
+# PARA UNIR DOS CONJUNTOS SE UTILIZA .UNION(SET) O (|)
+set_c = set_a.union(set_b)
+print(set_c)
+set_c = (set_a | set_b)
+print(set_c)
+
+# INTERSECIÓN ENTRE CONJUNTOS con (.intersection) o (&)
+set_d = set_a.intersection(set_b)
+print(set_d)
+set_d = (set_a & set_b)
+print(set_d)
+
+# DIFERENCIA DE ELEMENTOS DE UN CONJUNTO AL OTRO (DIFERENCE) O (-)
+set_e = set_a.difference(set_b)
+print(set_e)
+set_e = (set_a - set_b)
+print(set_e)
+
+# DIFERENCIA SIMETRICA CON (SYMMETRIC_DIFFERENCE) HACER UNA UNION SIN LOS ELEMENTOS QUE TIENEN EN COMUN o (^)
+
+set_1 = set_a.symmetric_difference(set_b)
+print(set_1)
+set_1 = (set_a ^ set_b)
+print(set_1)
